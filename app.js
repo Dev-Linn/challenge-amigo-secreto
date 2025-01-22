037dcb7615c7c4d40e597c9cdcf519d91862b98f
Lincoln <linnsilva3636@gmail.com>
2025-01-22 02:05:44 -0300

    finish

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-let nomes = []
+let amigos = []
 
 function adicionarAmigo(){
     const nomeDoAmigo = document.getElementById("amigo");
     const nome = nomeDoAmigo.value.trim();
+
+    if (nome == ""){
+        alert("amigao, coloca um nome ai");
+        return;
+    }
+
+    amigos.push(nome);
+    console.log(amigos);
+
+    nomeDoAmigo.value = "";
+    exibirAmigos();
 }
 
+function exibirAmigos(){
+    const lista = document.querySelector("#listaAmigos"); 
+    lista.innerHTML = "";
+    console.log("Lista limpa")
+
+    for (let i = 0; i < amigos.length; i++){
+        const li = document.createElement("li")
+        li.textContent = amigos[i]
+        lista.appendChild(li);
+    }
+}
+function sortearAmigo() {
+    if (amigos.length < 2 || amigos.length === 0) { 
+        alert("Não há amigos para sortear. Adicione alguns nomes.");
+        return;
+    } 
+ 
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    const resultado = document.querySelector("#resultado");
+    
+    resultado.innerHTML = `O amigo secreto sorteado é: <strong>${amigoSorteado}</strong>`;
+}
