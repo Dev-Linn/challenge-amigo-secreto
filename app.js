14ce6a09be7df37a48ef16cd0166c0d6cac6f2b9
Lincoln <linnsilva3636@gmail.com>
2025-01-22 01:31:56 -0300

    função adicionaAmigo

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = []
+
+function adicionarAmigo(){
+    const nomeDoAmigo = document.getElementById("amigo");
+    const nome = nomeDoAmigo.value.trim();
+}
+
